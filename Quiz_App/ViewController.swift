fc9d7644ca966b28e80b7f94a33e9e0c10303e0c
Melih Özmen <melihozmen9@gmail.com>
2022-12-02 11:45:38 +0300

    Initial Commit

---
+++ Quiz_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz_App
+//
+//  Created by Kullanici on 2.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
