a86d5750e08fbcb859cb2f6ec505f1ad29d588a1
Melih Özmen <melihozmen9@gmail.com>
2022-12-02 17:24:59 +0300

    Tasarım yapıldı.

--- Quiz_App/ViewController.swift
+++ Quiz_App/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    var questionNumber = 0
+    let quiz : [String] = ["Four plus two equals to six.","Five minus 1 equals to four.", "Nine divided by three equals to three."]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        questionLabel.text = quiz[questionNumber]
+        
     }
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNumber += 1
+        questionLabel.text = quiz[questionNumber]
+    }
+    
 
 }
 
