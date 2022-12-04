9120f2f4988ddc712ec2c591aa89bb86d4e4cfbc
Melih Özmen <melihozmen9@gmail.com>
2022-12-04 20:27:18 +0300

    Update ViewController.swift

--- Quiz_App/ViewController.swift
+++ Quiz_App/ViewController.swift
@@ -13,17 +13,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     var questionNumber = 0
-    let quiz : [String] = ["Four plus two equals to six.","Five minus 1 equals to four.", "Nine divided by three equals to three."]
+    let quiz : [[String]] = [["Four plus two equals to six.","True"],["Five minus 1 equals to four.","True"], ["Nine divided by three equals to two.","False"]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        questionLabel.text = quiz[questionNumber]
-        
+        questionLabel.text = quiz[questionNumber][0]
+      
     }
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        let actualAnswer = quiz[questionNumber][1]
+        if userAnswer == actualAnswer {
+            print("Right!")
+        } else {
+            print("Wrong!")
+        }
+        if questionNumber < quiz.count-1 {
         questionNumber += 1
-        questionLabel.text = quiz[questionNumber]
+        } else {
+            questionNumber = 0
+            questionLabel.text = "Quiz Finished. Thank you for participation."
+        }
+        questionLabel.text = quiz[questionNumber][0]
     }
     
 
