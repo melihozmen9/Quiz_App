89693945f9bd0087a46e0999301378dbbfe4bed3
Melih Özmen <melihozmen9@gmail.com>
2022-12-07 23:39:11 +0300

    DesignPattern
    MVC design pattern'i oluşturuldu.

---
+++ Quiz_App/Controller/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Quiz_App
+//
+//  Created by Kullanici on 2.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    var quizBrain = QuizBrain()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        updateUI()
+    }
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight == true {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestions()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI(){
+        questionLabel.text = quizBrain.getQuestionText()
+        scoreLabel.text = "Score : \(quizBrain.getScore())"
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = quizBrain.getProgress()
+    }
+    
+    
+}
+
---
+++ Quiz_App/Model/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  Quiz_App
+//
+//  Created by Kullanici on 7.12.2022.
+//
+
+import Foundation
+struct Question {
+    let text : String
+    let answer : String
+    init(q:String,a:String) {
+        text = q
+        answer = a
+    }
+}
